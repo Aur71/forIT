4d6cf6f63bd7e4459f64d4e1c68a766a219269f2
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-11-26 08:34:21 +0200

    3d model added

--- src/data/cases.jsx
+++ src/data/cases.jsx
@@ -1,19 +1,18 @@
-import fractal from '../assets/cases/case-1/fractal.png'
-import corsairIcue from '../assets/cases/case-2/corsair_icue.png'
+import case1 from '../assets/style/case-1/fractal_meshify.png'
 
 export const cases = [
   {
     id: 1,
     name: 'Fractal',
-    img: fractal,
+    img: case1,
     info: 'ceva',
-    path: `src/assets/cases/case-1/scene.gltf`,
+    path: `src\assets\style\case-1\scene.gltf`,
   },
 
   {
     id: 2,
-    name: 'Fractal',
-    img: corsairIcue,
+    name: 'Second case',
+    img: '',
     info: 'ceva',
     path: `src/assets/cases/case-2/scene.gltf`,
   },
--- src/pages/build your own/pc - style/Showcase.jsx
+++ src/pages/build your own/pc - style/Showcase.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, Stage, PresentationControls } from '@react-three/drei'
+
+function Model(props) {
+  const { scene } = useGLTF('src\\assets\\style\\case-1\\scene.gltf')
+  return <primitive object={scene} {...props} />
+}
 
 const Showcase = ({ caseShowcase, setCaseShowcase, setFullSpec }) => {
   return (
     <div className={`showcase ${caseShowcase.id && 'active'}`}>
+      <div className='model-showcase'>
+        <Canvas dpr={[1, 2]}>
+          <PresentationControls speed={1.5}>
+            <Stage>
+              <Model />
+            </Stage>
+          </PresentationControls>
+        </Canvas>
+      </div>
+
       <div className='btn-container'>
         <button
           className='full-spec-btn'
