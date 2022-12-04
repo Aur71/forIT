d1d59ddcb0858192cf1e2aa961ff30e98a01430f
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-04 15:14:50 +0200

    3d Model bug

--- src/pages/build your own/pc - style/Showcase.jsx
+++ src/pages/build your own/pc - style/Showcase.jsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { useGLTF, Stage, PresentationControls } from '@react-three/drei'
-
-function Model() {
-  const { scene } = useGLTF('/src/assets/style/case-1/scene.gltf')
-  return <primitive object={scene} />
-}
 
 const Showcase = ({ setFullSpec, showCase, setShowCase, activeCase }) => {
-
-
   return (
     <div className={`showcase  ${showCase && 'active'}`}>
-      <div className='model-showcase'>
-        <Canvas dpr={[1, 2]}>
-          <PresentationControls speed={1.5}>
-            <Stage>
-              <Model />
-            </Stage>
-          </PresentationControls>
-        </Canvas>
-      </div>
+      <div className='model-showcase'></div>
 
       <div className='btn-container'>
         <button className='full-spec-btn' onClick={() => setFullSpec(true)}>
